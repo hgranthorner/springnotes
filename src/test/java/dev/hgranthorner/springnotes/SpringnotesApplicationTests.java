646b92bf55be0ba77a4721b37962e70345d1729d
Grant Horner <h.grant.horner@gmail.com>
2022-11-30 08:37:52 -0500

    initial commit

---
+++ src/test/java/dev/hgranthorner/springnotes/SpringnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hgranthorner.springnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
